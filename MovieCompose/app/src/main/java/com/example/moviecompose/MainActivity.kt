30656623efd3fb03a220f8c50ffb356b02c031ff
Autumn Lauen <autumn.lauen@banno.com>
2021-06-03 15:09:52 -0500

    Adds the list of movies to the movie search screen

--- MovieCompose/app/src/main/java/com/example/moviecompose/MainActivity.kt
+++ MovieCompose/app/src/main/java/com/example/moviecompose/MainActivity.kt
@@ -1,34 +1,11 @@
 package com.example.moviecompose
 
 import android.os.Bundle
-import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import com.example.moviecompose.ui.theme.MovieComposeTheme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
-val testMovies = listOf(
-    Movie("Oceans 8"),
-    Movie("John Tucker Must Die"),
-    Movie("Clueless"),
-    Movie("Oceans 8"),
-    Movie("John Tucker Must Die"),
-    Movie("Clueless")
-)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,32 +18,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-data class Movie(
-    val title: String
-)
-
-@Composable
-fun MovieList(
-    movies: List<Movie>
-) {
-    LazyColumn {
-        items(movies) { movie ->
-            MovieRow(movie)
-        }
-    }
-}
-
-@Composable
-fun MovieRow(movie: Movie) {
-    Row {
-        Image(
-            painter = painterResource(id = android.R.drawable.btn_star_big_on),
-            contentDescription = "star",
-            modifier = Modifier.size(80.dp, 80.dp)
-        )
-        Text(text = movie.title, fontSize = 20.sp, modifier = Modifier.align(Alignment.CenterVertically))
     }
 }
--- MovieCompose/app/src/main/java/com/example/moviecompose/searchmovies/SearchMoviesScreen.kt
+++ MovieCompose/app/src/main/java/com/example/moviecompose/searchmovies/SearchMoviesScreen.kt
@@ -3,6 +3,8 @@ package com.example.moviecompose.searchmovies
 import android.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -20,6 +22,8 @@ import androidx.compose.ui.unit.sp
 fun SearchMoviesScreen() {
 
     val text = remember { mutableStateOf(TextFieldValue()) }
+
+    val listOfMovies = remember { mutableStateOf(emptyList<Movie>())}
 
     Column(
         modifier = Modifier
@@ -45,8 +49,54 @@ fun SearchMoviesScreen() {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
-            onClick = { /*TODO call api with search text and display results*/ }) {
+            onClick = { listOfMovies.value = testMovies }) {
             Text(text = "Search", fontSize = 20.sp)
         }
+        MovieList(movies = listOfMovies.value)
+    }
+}
+
+val testMovies = listOf(
+    Movie("Oceans 8"),
+    Movie("John Tucker Must Die"),
+    Movie("Clueless"),
+    Movie("Oceans 8"),
+    Movie("John Tucker Must Die"),
+    Movie("Clueless")
+)
+
+data class Movie(
+    val title: String
+)
+
+@Composable
+fun MovieList(
+    movies: List<Movie>?
+) {
+    if (movies == null) {
+        Column() {
+        }
+    } else {
+        LazyColumn {
+            items(movies) { movie ->
+                MovieRow(movie)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: Movie) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Image(
+            painter = painterResource(id = android.R.drawable.btn_star_big_on),
+            contentDescription = "star",
+            modifier = Modifier.size(80.dp, 80.dp)
+        )
+        Text(
+            text = movie.title,
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
     }
 }
