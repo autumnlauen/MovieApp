648423a93a0b0bf32e5baf280030b2553f8e9592
Autumn Lauen <autumn.lauen@banno.com>
2021-06-03 14:14:00 -0500

    Successfully adds the searchbar on the movie search screen

--- MovieCompose/app/src/main/java/com/example/moviecompose/searchmovies/SearchMoviesScreen.kt
+++ MovieCompose/app/src/main/java/com/example/moviecompose/searchmovies/SearchMoviesScreen.kt
@@ -1,19 +1,38 @@
 package com.example.moviecompose.searchmovies
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview
 fun SearchMoviesScreen() {
+
+    val text = remember { mutableStateOf(TextFieldValue()) }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Hi! This will eventually be the movie search screen.")
+        TextField(
+            value = text.value,
+            leadingIcon = {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_menu_search),
+                    contentDescription = "search"
+                )
+            },
+            onValueChange = { text.value = it },
+            label = { Text(text = "Search by movie") })
     }
 }
